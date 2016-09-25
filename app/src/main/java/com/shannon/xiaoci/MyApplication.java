baf7eb62eb3c928a1072dcf46351b9dea50bf2b7
Violet <xinbai16@student.hh.se>
2016-09-25 15:19:12 +0800

    Initial commit

---
+++ app/src/main/java/com/shannon/xiaoci/MyApplication.java
@@ -0,0 +1,34 @@
+package com.shannon.xiaoci;
+
+import android.app.Application;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.shannon.xiaoci.main.model.WordModel;
+
+/**
+ * Created by Baixingyue on 2016/9/25.
+ */
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        init();
+
+        System.out.println("sd卡的路径：" + WordModel.DATABASE_PATH);
+
+
+
+
+
+    }
+
+    private void init() {
+
+        ImageLoaderConfiguration ilc = new ImageLoaderConfiguration.Builder(getApplicationContext()).build();
+        ImageLoader.getInstance().init(ilc);
+
+    }
+}
